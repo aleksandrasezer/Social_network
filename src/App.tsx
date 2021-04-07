2575ecaaf83ae8704691682f67ba76b8b689ef53
aleksandrasezer <korelskaia@gmail.com>
2021-04-07 13:27:42 +0300

    06.04.2021

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,23 @@ import './App.css';
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Profile from "./components/profile/Profile";
+import Dialogs from "./components/dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="app_wrapper">
-      <div className="header">
-          <Header />
-      </div>
-      <div className="navbar">
-          <Navbar />
-      </div>
-      <div className="profile">
-          <Profile />
-      </div>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="app_wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app_wrapper_content">
+                    <Route path='/dialogs' component={Dialogs} />
+                    <Route path='/profile' component={Profile} />
+                </div>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import s from "./Navbar.module.css";
 
 function Navbar() {
-    return <div className="navbar">
-        <div>Profile</div>
-        <div>Messages</div>
-        <div>News</div>
-        <div>Music</div>
-        <div>Settings</div>
+    return <div className={s.navbar}>
+        <div><a href="/profile">Profile</a></div>
+        <div><a href="/dialogs">Messages</a></div>
+        <div><a href="/feed">News</a></div>
+        <div><a href="/music">Music</a></div>
+        <div><a href="/settings">Settings</a></div>
     </div>
 }
 
