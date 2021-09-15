a59cb78b6ad929ade79b46b3ebb51e527462d36f
aleksandrakorelskaia <korelskaia@gmail.com>
2021-09-15 23:45:53 +0300

    css navbar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import s from "./Header.module.css";
 import {NavLink} from "react-router-dom";
 import {InitStateType} from "../../redux/auth-reducer";
-import {Button} from "../Button/Button";
 
 type HeaderPropsType = {
     data: InitStateType
@@ -13,12 +12,23 @@ function Header(props: HeaderPropsType) {
     return (
         <div className={s.header}>
 
-            <img src="https://www.clipartkey.com/mpngs/m/67-676200_clip-art-pinterest-rick-and-morty-png.png" alt="logo"/>
+            <img src="https://www.clipartkey.com/mpngs/m/67-676200_clip-art-pinterest-rick-and-morty-png.png"
+                 alt="logo"/>
 
             <div className={s.loginBlock}>
                 {props.data.isAuth
 
-                    ? <div>{props.data.login} <Button onClick={props.logout}> Log out </Button></div>
+                    ? <div>
+                        <NavLink to="/profile">
+                            <div className={s.userName}>
+                            {props.data.login}
+                        </div>
+                        </NavLink>
+
+                        <div className={s.logOut} onClick={props.logout}>
+                            Log out
+                        </div>
+                    </div>
 
                     : <NavLink to={'/login'}>Login</NavLink>
 
